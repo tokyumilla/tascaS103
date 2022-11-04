e2ab762524acde8c5efd1b59d95db2d28f9814b4
tokyumilla <tokyumilla@gmail.com>
2022-11-04 12:00:05 +0100

    Ejercicio 1 hecho a la espera de corrección

--- src/n1exercici1/Main.java
+++ src/n1exercici1/Main.java
@@ -1,5 +1,48 @@
+package n1exercici1;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Month> months = new ArrayList<>();
+        Month enero = new Month("enero");
+        Month febrero = new Month("febrero");
+        Month marzo = new Month("marzo");
+        Month abril = new Month("abril");
+        Month mayo = new Month("mayo");
+        Month junio = new Month("junio");
+        Month julio = new Month("julio");
+        Month agosto = new Month ("agosto");
+        Month septiembre = new Month("septiembre");
+        Month octubre = new Month("octubre");
+        Month noviembre = new Month("noviembre");
+        Month diciembre = new Month("diciembre");
+
+        months.add(enero);
+        months.add(febrero);
+        months.add(marzo);
+        months.add(abril);
+        months.add(mayo);
+        months.add(junio);
+        months.add(julio);
+        months.add(septiembre);
+        months.add(octubre);
+        months.add(noviembre);
+        months.add(diciembre);
+
+
+        months.add(7,agosto);
+
+        System.out.println(months);
+
+        Collection<Month> monthsHashSet = new HashSet<Month>(months);
+
+        if (!monthsHashSet.add(junio)) {
+            System.out.println("No se puede añadir junio porque ya está en la lista");
+        }
+        System.out.println(monthsHashSet);
+
+
     }
 }
--- src/n1exercici1/Month.java
+++ src/n1exercici1/Month.java
@@ -1,2 +1,16 @@
-package n1exercici1;public class Month {
+package n1exercici1;
+
+public class Month {
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
